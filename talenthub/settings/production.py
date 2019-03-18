d4d086e8ebcae68ae7084730963468bb413e50e6
Kajetan <kajetan.boroszko@gmail.com>
2019-03-18 13:42:35 +0100

    setup to use postgres

--- talenthub/settings/production.py
+++ talenthub/settings/production.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'talenthub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'circle_test',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USERNAME': 'root',
+        'PASSWORD': ''
     }
 }
 
