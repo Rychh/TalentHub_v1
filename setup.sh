35712d70b2124a9f6e2fb2d6f2c08415809a8b0e
kboroszko <kboroszko@users.noreply.github.com>
2019-03-19 20:50:09 +0100

    renamed start to setup and removed runserver

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 
 python3 manage.py makemigrations talenthub
 python3 manage.py migrate
-echo "import examples" | python3 manage.py shell 
-python3 manage.py runserver
+echo "import examples" | python3 manage.py shell
