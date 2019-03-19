107de5652dfbed54f31c91db5b5c407dba321c9d
Rychh <32704674+Rychh@users.noreply.github.com>
2019-03-19 18:13:08 +0100

    Revert "Start script"

--- README.md
+++
@@ -1,6 +0,0 @@
-# TalentHub_v1
-
-How to install TalentHub locally:
-- git clone
-- pip install -r requirements.txt
-- ./start
--- talenthub/settings/__init__.py
+++ talenthub/settings/__init__.py
@@ -1,11 +1,8 @@
+from .base import *
 from .production import *
 
-try:
-    from .local_default import *
-except:
-	pass
 
 try:
     from .local import *
 except:
-    pass
+	pass
