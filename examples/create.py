05d9cb5d81c5ab041f4930e28780dc8110720843
Kajetan <kajetan.boroszko@gmail.com>
2019-04-16 11:45:34 +0200

    improved example generation

--- examples/create.py
+++ examples/create.py
@@ -42,9 +42,7 @@ if Profile.objects.count() < 2:
     p2 = Profile.objects.create(user=u2, first_name='dumb_name',
                                 last_name='dumb_last_name',
                                 age=54, balance=120)
-else:
-    p = Profile.objects.all()[0]
-    p2 = Profile.objects.all()[1]
+
 
 cat = []
 for name in categories:
@@ -53,6 +51,8 @@ for name in categories:
 n1 = timezone.now() - timedelta(minutes=random.randint(1, 3000))
 n2 = timezone.now() - timedelta(minutes=random.randint(3000, 9000))
 
+pc = Profile.objects.all().count()
+
 for i in range(100):
     n1 = timezone.now() - timedelta(minutes=random.randint(1, 3000))
     n2 = timezone.now() - timedelta(minutes=random.randint(3000, 9000))
@@ -60,7 +60,7 @@ for i in range(100):
     o = Offer.objects.create(description=desc[i % len(desc)],
                              price=random.randint(50, 150),
                              category=cat[i % len(cat)],
-                             user_profile=p,
+                             user_profile=Profile.objects.all()[i % pc],
                              avaliability=[[n1, n2]])
     o.tag.add(tags[i * 13 % 100])
     o.tag.add(tags[i * 19 % 100])
@@ -72,15 +72,14 @@ ms1.save()
 ms2 = MeetingStatus(name='agreed')
 ms2.save()
 
-for i in range(10):
-    oc = Offer.objects.count()
-    o1 = Offer.objects.all()[i % oc]
-    o2 = Offer.objects.all()[(17 * i + 1) % oc]
-    m1 = Meeting(date=n1, agreed_price=10,
-                 status=ms1, student=p,
-                 teacher=p2, offer=o1)
-    m1.save()
-    m2 = Meeting(date=n1, agreed_price=10,
-                 status=ms2, student=p2,
-                 teacher=p, offer=o2)
-    m2.save()
+for p in Profile.objects.all():
+    ofers = Offer.objects.filter(user_profile=p)
+    mss = [ms1, ms2]
+    for i in range(min(5, ofers.count())):
+        other_profiles = Profile.objects.exclude(user=p.user)
+        opc = other_profiles.count()
+        op = other_profiles[random.randint(0, opc - 1)]
+        m = Meeting(date=n1, agreed_price=10,
+                    status=mss[i % 2], student=p,
+                    teacher=op, offer=ofers[i])
+        m.save()
